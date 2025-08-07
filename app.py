35e3eb88d9cbeb2c3fb95033a105893b66450f30
administrator@WEST.LOCAL <administrator@WEST.LOCAL>
2025-08-07 14:45:02 -0700

    change server to https

--- app.py
+++ app.py
@@ -39,6 +39,6 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 5000))
     # app.run(debug=True, port=port)
-    app.run(host='0.0.0.0', debug=True, port=port)
+    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context='adhoc')
 
 
